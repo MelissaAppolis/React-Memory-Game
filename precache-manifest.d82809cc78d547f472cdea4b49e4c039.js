5128c478934d351f68bdb459fd0f04571ce1af17
Melissa <melissa@Ambers-MacBook-Air.local>
2020-10-01 11:58:14 +0200

    Updates

---
+++ precache-manifest.d82809cc78d547f472cdea4b49e4c039.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc1557d6ab974e8ab65e574265303ab5",
+    "url": "/React-Memory-Game/index.html"
+  },
+  {
+    "revision": "5e715ba186e59326d443",
+    "url": "/React-Memory-Game/static/css/main.2b5036ad.chunk.css"
+  },
+  {
+    "revision": "739d9ad7323aa6ed3d74",
+    "url": "/React-Memory-Game/static/js/2.f856cab9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Memory-Game/static/js/2.f856cab9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e715ba186e59326d443",
+    "url": "/React-Memory-Game/static/js/main.e66f7ff8.chunk.js"
+  },
+  {
+    "revision": "05cc0cac4fe663fe7d4f",
+    "url": "/React-Memory-Game/static/js/runtime-main.e257704f.js"
+  }
+]);
